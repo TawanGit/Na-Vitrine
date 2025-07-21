a48eb49c3fb44ec45b4ba9c682b73612c8a878de
tawan <thiago_tawan@outlook.com>
2025-07-21 16:27:29 -0300

    feat: creation email service

---
+++ src/main/java/com/tawangit/agregate/service/EmailService.java
@@ -0,0 +1,20 @@
+package com.tawangit.agregate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendInviteEmail(String to, String inviteLink) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Você foi convidado para se cadastrar");
+        message.setText("Olá! Você foi convidado para se cadastrar. Use o link abaixo:\n" + inviteLink);
+        mailSender.send(message);
+    }
+    }

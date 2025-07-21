e7b7415e85c233c6e06898a3140b9974f418d379
tawan <thiago_tawan@outlook.com>
2025-07-20 21:10:05 -0300

    first commit

---
+++ src/main/java/com/tawangit/agregate/AgregateApplication.java
@@ -0,0 +1,13 @@
+package com.tawangit.agregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tawangit/agregate/AgregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawangit.agregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

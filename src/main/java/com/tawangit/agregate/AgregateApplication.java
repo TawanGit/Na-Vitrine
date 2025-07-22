bcc74bff372edf64b084faed7ee3ee497de8d1d1
tawan <thiago_tawan@outlook.com>
2025-07-22 17:31:25 -0300

    feat: enabling async

--- src/main/java/com/tawangit/agregate/AgregateApplication.java
+++ src/main/java/com/tawangit/agregate/AgregateApplication.java
@@ -2,7 +2,9 @@ package com.tawangit.agregate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class AgregateApplication {
 

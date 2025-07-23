381c3ecd162afb667b26d6199b6cd267e13db310
tawan <thiago_tawan@outlook.com>
2025-07-22 21:16:49 -0300

    feat: adding global exception

---
+++ src/main/java/com/tawangit/agregate/infra/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.tawangit.agregate.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice // vai capturar exceções de todos os controllers
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.ok().body(errors);
+    }
+}
